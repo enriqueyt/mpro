4a1621e880bfaa97425abce6b70844f3ee2b3774
Daniel Ramirez <fleadanielito007@gmail.com>
2018-01-24 22:36:41 -0400

    #F Bug on createUniqueId method was fixed

--- libs/utils.js
+++ libs/utils.js
@@ -30,13 +30,47 @@ var Utils = {
       var time = Date.now();
       var last = now.last || time;
       return now.last = time > last ? time : last + 1;
-    }
+    };
+    var chr4 = function () {
+      return Math.random().toString(16).slice(-4);
+    };
     
-    return (prefix || '') + pid + now().toString(36);
+    return (prefix || '') + pid + now().toString(36) + chr4();
   },
 
   formatDate: function (date, format) {
     return Moment(date.toString()).format(format);
+  },
+
+  getStartDate: function (date) {
+    var newDate = new Date(date);
+    
+    newDate.setHours(0);
+    newDate.setMinutes(0);
+    newDate.setSeconds(0);
+    newDate.setMilliseconds(0);
+
+    return newDate;
+  },
+
+  getEndDate: function (date) {
+    var newDate = new Date(date);
+    
+    newDate.setHours(23);
+    newDate.setMinutes(59);
+    newDate.setSeconds(59);
+    newDate.setMilliseconds(999);
+
+    return newDate;
+  },
+
+  getLimitDates: function (date) {
+    var limitDates = {
+      min: Utils.getStartDate(date),
+      max: Utils.getEndDate(date)
+    };
+    
+    return limitDates;
   }
 };
 
