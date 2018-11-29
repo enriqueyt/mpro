a6d2f6cc2899546d1065608a1fa7ae248858f0f0
enrique yepez <enriqueyt@gmail.com>
2018-11-29 18:17:39 -0500

    add the notifications

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ var path = require('path');
 
 var dbName = process.env.DB_PATH || 'db.test.json';
 
-module.exports.dbUrl = path.resolve(__dirname, dbName);
+module.exports.dbUrl = path.resolve('config/', dbName);
 module.exports.dbPath = require(module.exports.dbUrl);
